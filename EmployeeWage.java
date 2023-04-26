659ec7a8dbcdd06d84b47c81a7097580e9e00b41
PrasannaMohanta <pmohanta53@gmail.com>
2023-04-26 08:37:23 +0530

    Added:WelcomeToEmployeeWage

---
+++ EmployeeWage.java
@@ -0,0 +1,20 @@
+package com.Bridgelabz;
+/**
+ * 
+ * @author pmoha
+ *
+ */
+public class EmployeeWage {
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		/*
+		 * welcome print
+		 */
+		System.out.println("welcome to EmployeeWage");
+	}
+
+}
